af274face776a1d53e3e7504c8ffac7d221ddfce
Benjamin Guichard <ben.guichard@live.fr>
2017-01-21 01:50:16 +0100

    Connection to HSQLDB

--- src/m1/efrei/rest/Movies.java
+++ src/m1/efrei/rest/Movies.java
@@ -8,6 +8,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 //Sets the path to base URL + /hello
 @Path("/movies")
@@ -17,7 +21,35 @@ public class Movies {
     @GET
     @Produces(MediaType.TEXT_XML)
     public String moviesList() {
-        return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+
+        Connection connec;
+        Statement stmt = null;
+        ResultSet result = null;
+
+        try {
+            // CONNECT to the database
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+            connec = DriverManager.getConnection("jdbc:hsqldb:file:L:/Java/RestJee/HSQLDB/", "root", "root");
+            stmt = connec.createStatement();
+
+            // READ into the database
+            result = stmt.executeQuery("SELECT * FROM MOVIES");
+            connec.close();
+
+            // CONVERT to xml format
+            while (result.next()) {
+                // TODO Parse to XML
+                System.out.println(result.getInt("ID") + " | " +
+                        result.getString("TITLE") + " | " +
+                        result.getInt("YEAR")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+
+        // SEND the XML
+        return "<?xml version=\"1.0\"?>" + "<hello> QUOI?" + "</hello>";
     }
 
 }
